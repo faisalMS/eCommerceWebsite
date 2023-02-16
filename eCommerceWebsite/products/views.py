9a27f73f78ecd98650f1858753a5137d771938ce
FaisalMS <fis445@hotmail.com>
2023-02-16 11:42:03 +0300

    first

---
+++ eCommerceWebsite/products/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'products/home.html')
+
+
+def products(request):
+    return render(request, 'products/products.html')
