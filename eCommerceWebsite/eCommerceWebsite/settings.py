317f345d7a56d28fc5e696b09d67e0a37b19ea3f
FaisalMS <fis445@hotmail.com>
2023-02-18 06:29:19 +0300

    Version 0.2

--- eCommerceWebsite/eCommerceWebsite/settings.py
+++ eCommerceWebsite/eCommerceWebsite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'products'
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -116,7 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'eCommerceWebsite/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
